d0e6c2cbcae10215bcca3716bd83469b0b892a9b
neeew-coder <markkevinvillaflor25@gmail.com>
2025-09-01 16:15:33 +0800

    Update progress.js

--- backend/models/progress.js
+++ backend/models/progress.js
@@ -3,9 +3,8 @@ const mongoose = require("mongoose");
 const progressSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, required: true, unique: true },
   completed: {
-    type: Map,
-    of: [String],
-    default: () => new Map()
+    type: Object,
+    default: () => ({})
   }
 });
 
